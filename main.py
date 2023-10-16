f8767d7807d182aae34e442f3f37097073d85f6b
MihailBurdin <144234023+MihailBurdin@users.noreply.github.com>
2023-10-16 11:10:52 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+#Задание №4
+#x = int(input())
+
+#if x < -20 :
+    #print(0,'#Холодно')
+#elif -20 < x < 0:
+    #print(1,'#Прохладно')
+#elif 0 < x < 15:
+    #print(2,'#Зябко')
+#elif 15 < x < 25:
+    #print(3,'#Тепло')
+#else  :
+    #print(4,'#Жарко')
+
+#Задание №3
+x = int(input())
+y = int(input())
+z = int(input())
+limit = int(input())
+if x > limit:
+    print(limit)
+else:
+    print()
